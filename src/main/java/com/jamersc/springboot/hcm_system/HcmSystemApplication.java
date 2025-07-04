a4e3596e3df5926d1d483c6de6a6ba03aa5f6bc5
Jamer Catalla <mr.catalla28@gmail.com>
2025-07-05 00:01:13 +0800

    2nd commit

---
+++ src/main/java/com/jamersc/springboot/hcm_system/HcmSystemApplication.java
@@ -0,0 +1,14 @@
+package com.jamersc.springboot.hcm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcmSystemApplication.class, args);
+		//System.out.println("Hello, world!");
+	}
+
+}
---
+++ src/test/java/com/jamersc/springboot/hcm_system/HcmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamersc.springboot.hcm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
