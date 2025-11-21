01c0c1f0a71d9ffb7964d518c91e71245f8af687
Jamer Catalla <mr.catalla28@gmail.com>
2025-11-21 19:38:33 +0800

    feature: implement custom api response in applicant controller

--- src/main/java/com/jamersc/springboot/hcm_api/controller/ApplicantController.java
+++ src/main/java/com/jamersc/springboot/hcm_api/controller/ApplicantController.java
@@ -6,11 +6,13 @@ import com.jamersc.springboot.hcm_api.dto.application.ApplicationResponseDto;
 import com.jamersc.springboot.hcm_api.dto.job.JobResponseDto;
 import com.jamersc.springboot.hcm_api.service.applicant.ApplicantService;
 import com.jamersc.springboot.hcm_api.service.job.JobService;
+import com.jamersc.springboot.hcm_api.utils.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +20,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
@@ -33,31 +36,62 @@ public class ApplicantController {
     }
 
     @GetMapping("/open/jobs")
-    public ResponseEntity<Page<JobResponseDto>> getOpenJobs(
+    public ResponseEntity<ApiResponse<Page<JobResponseDto>>> getOpenJobs(
             @PageableDefault(page = 0, size = 10, sort = "title") Pageable pageable) {
         Page<JobResponseDto> listOfOpenJobs = jobService.getOpenJobs(pageable);
-        return new ResponseEntity<>(listOfOpenJobs, HttpStatus.OK);
+        ApiResponse<Page<JobResponseDto>> response = ApiResponse.<Page<JobResponseDto>>builder()
+                .success(true)
+                .message("List of open jobs retrieved successfully!")
+                .data(listOfOpenJobs)
+                .status(HttpStatus.OK.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/jobs/{id}/apply")
-    public ResponseEntity<ApplicationResponseDto> applyForJob(
+    public ResponseEntity<ApiResponse<ApplicationResponseDto>> applyForJob(
             @PathVariable Long id, Authentication authentication) {
-        ApplicationResponseDto application = applicantService.applyForJob(id, authentication);
-        return new ResponseEntity<>(application, HttpStatus.CREATED);
+        ApplicationResponseDto applicationSubmitted = applicantService.applyForJob(id, authentication);
+        ApiResponse<ApplicationResponseDto> response = ApiResponse.<ApplicationResponseDto>builder()
+                .success(true)
+                .message("Job application submitted successfully!")
+                .data(applicationSubmitted)
+                .status(HttpStatus.CREATED.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/applications/jobs-applied")
-    public ResponseEntity<Page<ApplicationResponseDto>> getAllApplicantJobsApplied(
+    public ResponseEntity<ApiResponse<Page<ApplicationResponseDto>>> getAllApplicantJobsApplied(
             @PageableDefault(page = 0, size = 10, sort = "status") Pageable pageable, Authentication authentication) {
         Page<ApplicationResponseDto> appliedJobs = applicantService.getAllApplicantJobsApplied(pageable, authentication);
-        return new ResponseEntity<>(appliedJobs, HttpStatus.OK);
+        ApiResponse<Page<ApplicationResponseDto>> response = ApiResponse.<Page<ApplicationResponseDto>>builder()
+                .success(true)
+                .message("Applicant jobs applied retrieved successfully!")
+                .data(appliedJobs)
+                .status(HttpStatus.OK.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/application/{id}/view")
-    public ResponseEntity<Optional<ApplicationResponseDto>> getAppliedJob(
+    public ResponseEntity<ApiResponse<Optional<ApplicationResponseDto>>> getAppliedJob(
             @PathVariable Long id, Authentication authentication) {
-        Optional<ApplicationResponseDto> application = applicantService.getApplicantJobsAppliedById(id, authentication);
-        return new ResponseEntity<>(application, HttpStatus.OK);
+        Optional<ApplicationResponseDto> appliedJob = applicantService.getApplicantJobsAppliedById(id, authentication);
+        ApiResponse<Optional<ApplicationResponseDto>> response = ApiResponse.<Optional<ApplicationResponseDto>>builder()
+                .success(true)
+                .message("Job application submitted successfully!")
+                .data(appliedJob)
+                .status(HttpStatus.CREATED.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/application/{id}/withdraw")
